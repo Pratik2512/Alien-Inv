f702d8a396e6364736ae7a357237039e240d1353
Pratik <pratikmohite1401@gmail.com>
2024-01-24 00:19:03 +0530

    Leveling up

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,7 +59,11 @@ class AlienInvasion:
 
     def _check_play_button(self,mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        # if self.play_button.rect.collidepoint(mouse_pos):
+        if button_clicked and not self.stats.game_active:
+            #Reset game settings.
+            self.settings.initialize_dynamic_settings
             #Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -71,6 +75,9 @@ class AlienInvasion:
             #Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
+
+            #Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -117,6 +124,7 @@ class AlienInvasion:
             #Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     
     def _create_fleet(self):
         """Create a fleet of aliens."""
@@ -188,6 +196,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screeen."""
